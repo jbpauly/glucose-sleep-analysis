76e8fa2ed3e5a2d7bf00f3e3f99137ec96a4a791
Joseph Pauly <49600051+jbpauly@users.noreply.github.com>
2020-11-17 14:30:40 -0600

    setup remote

---
+++ glucose-sleep-analysis/__init__.py
@@ -0,0 +1,5 @@
+"""glucose-sleep-analysis - Streamlit App to help you cross analyze your Whoop sleep and recovery data with your Levels glucose data"""
+
+__version__ = '0.1.0'
+__author__ = 'Joe Pauly <"">'
+__all__ = []
---
+++ setup.py
@@ -0,0 +1,39 @@
+import io
+import os
+import re
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="glucose-sleep-analysis",
+    version="0.1.0",
+    url="https://github.com/jbpauly/glucose-sleep-analysis",
+    license='MIT',
+
+    author="Joe Pauly",
+    author_email="",
+
+    description="Streamlit App to help you cross analyze your Whoop sleep and recovery data with your Levels glucose "
+                "data",
+    long_description=read("README.rst"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ],
+)

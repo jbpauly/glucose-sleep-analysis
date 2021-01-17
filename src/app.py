2a848416a2a2724da4034375d4ec73e61f219632
jbpauly <49600051+jbpauly@users.noreply.github.com>
2021-01-17 14:46:08 -0600

    Dynamic events (#7)
    * Cross analyze Whoop, Levels, and Zero data.
    
    * app update
    
    * cache updates

--- src/app.py
+++ src/app.py
@@ -1,11 +1,14 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import utilities as util
-from pytz import all_timezones
+import plot
 import pandas as pd
 from PIL import Image
+import levels as lv
+import whoop as wp
+import zero as zo
 
-default_tz_index = all_timezones.index('America/Chicago')
+
 sample_file_path = util.SRC_PATH / 'sample.csv'
 
 st.set_page_config(page_title='Glucose | Sleep',
@@ -46,43 +49,54 @@ if welcome_sb:
         components.iframe("https://www.loom.com/embed/62467100449b4c45bca5b603cfd573ac", height=430)
 
 if example_analysis_sb:
-    sample_dataset = pd.read_csv(sample_file_path, parse_dates=['Date'], index_col=0)
+    sample_dataset = pd.read_csv(sample_file_path, parse_dates=['Date'], index_col=0).round(2)
+    corr_matrix = util.corr_matrix(sample_dataset, 'Date').round(2)
+    corr_heatmap = plot.plotly_heatmap(corr_matrix)
+    st.plotly_chart(corr_heatmap, use_container_width=True)
     x_selection_sample, y_selection_sample, color_selection_sample = util.variables_for_plot(sample_dataset,
                                                                                              app_section='example')
     if x_selection_sample != '<select>' and y_selection_sample != '<select>':
-        sample_plot = util.create_scatter(sample_dataset, x_selection_sample, y_selection_sample,
-                                          color_selection_sample)
-        st.altair_chart(sample_plot, use_container_width=True)
+        scatter = plot.plotly_scatter(sample_dataset, x_selection_sample, y_selection_sample, color_selection_sample)
+        line = plot.plotly_line(sample_dataset, x_selection_sample, y_selection_sample, 'Date')
+        st.plotly_chart(scatter, use_container_width=True)
+        st.plotly_chart(line, use_container_width=True)
+        # sample_scatter = util.create_scatter(sample_dataset,
+        #                                      x_selection_sample,
+        #                                      y_selection_sample,
+        #                                      color_selection_sample)
+        # corr_matrix = util.create_correlation_matrix(sample_dataset, date_column='Date')
+        # sample_corr = util.create_matrix_heatmap(corr_matrix,
+        #                                          x_selection_sample,
+        #                                          y_selection_sample)
+        # st.altair_chart(sample_corr, use_container_width=True)
+        # st.altair_chart(sample_scatter, use_container_width=True)
 
-sleep_df = None
-glucose_df = None
+    # pr = util.profile_report(sample_dataset)
+    # st_profile_report(pr)
+
+fitness_scores = None
+metabolic_scores = None
+fasting_scores = None
 if analyze_data_sb:
     with st.beta_expander("Upload Data", expanded=False):
-        tz_select_col, tz_verify_col = st.beta_columns((2, 1))
-        time_zone = tz_select_col.selectbox("Select your timezone associated with your glucose data",
-                                            options=all_timezones,
-                                            index=default_tz_index
-                                            )
-        tz_verify_col.text("")
-        tz_verify_col.text("")
-        verified_tz = tz_verify_col.checkbox('Verify selected timezone', value=False)
-        sleep_col, glucose_col = st.beta_columns(2)
-        sleep_file = sleep_col.file_uploader("Upload Sleep Data", type=['csv'])
-        glucose_file = glucose_col.file_uploader("Upload Glucose Data", type=['csv'])
-
-        if sleep_file is not None and glucose_file is not None:
-            sleep_df = util.load_sleep_data(sleep_file)
-            if not verified_tz:
-                st.error('Please verify the selected timezone for your glucose data for accurate pairing with your '
-                         'sleep data.')
-            else:
-                glucose_df = util.load_glucose_data(glucose_file, time_zone)
+        whoop_col, levels_col, zero_col = st.beta_columns(3)
+        whoop_file = whoop_col.file_uploader("Upload Whoop Data", type=['csv'])
+        levels_file = levels_col.file_uploader("Upload Levels Data", type=['csv'])
+        zero_file = zero_col.file_uploader("Upload Zero Fasting Data", type=['csv'])
+        st.write(type(whoop_file))
+        if whoop_file is not None and levels_file is not None and zero_file is not None:
+            fitness_scores = wp.load_whoop_data(whoop_file)
+            metabolic_scores = lv.load_levels_data(levels_file)
+            fasting = zo.load_zero_data(zero_file)
+            fasting_scores = zo.all_fasts_stats(fasting)
 
     with st.beta_expander("Analyze Data", expanded=False):
-        if sleep_df is not None and glucose_df is not None:
-            all_data = util.create_analysis_dataset(sleep=sleep_df, glucose=glucose_df)
+        if fitness_scores is not None and metabolic_scores is not None and fasting_scores is not None:
+            all_metrics = util.create_metrics_dataset(fitness_scores=fitness_scores,
+                                                      metabolic_scores=metabolic_scores,
+                                                      fasting_scores=fasting_scores,)
 
-            x_selection, y_selection, color_selection = util.variables_for_plot(all_data, )
+            x_selection, y_selection, color_selection = util.variables_for_plot(all_metrics, )
             if x_selection != '<select>' and y_selection != '<select>':
-                plot = util.create_scatter(all_data, x_selection, y_selection, color_selection)
+                plot = util.create_scatter(all_metrics, x_selection, y_selection, color_selection)
                 st.altair_chart(plot, use_container_width=True)
---
+++ src/whoop.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import streamlit as st
+
+
+@st.cache(suppress_st_warning=True)
+def load_whoop_data(sleep_file) -> pd.DataFrame:
+    """
+    Load a Whoop daily summary CSV file and return a pandas DataFrame version of the file.
+    Only read in a subset of columns defined by the expected_cols parameter.
+    Args:
+        sleep_file: file to be converted to a DataFrame.
+
+    Returns: pandas DataFrame of daily Whoop summary data.
+    """
+
+    # TODO: split into 'read' and 'load' functions
+
+    expected_cols = ['Date',
+                     'Strain',
+                     'Recovery',
+                     'Sleep Score',
+                     'RHR',
+                     'Average HR',
+                     'Max HR',
+                     'Respiratory Rate',
+                     'HRV (ms)',
+                     'Sleep (hr)',
+                     'Sleep Start',
+                     'Sleep End']
+    try:
+        raw_sleep = pd.read_csv(sleep_file,
+                                header=0,
+                                parse_dates=['Date'],
+                                index_col='Date',
+                                usecols=expected_cols
+                                )
+    except ValueError:
+        st.error(f"""
+        Incorrect format of Whoop data CSV. Please make sure you uploaded the correct file.
+        \nThe following columns must be present in the first row:
+        \n {expected_cols}
+        """)
+        raise
+    raw_sleep['Sleep Start'] = raw_sleep['Sleep Start'].astype('datetime64[ms]')
+    raw_sleep['Sleep End'] = raw_sleep['Sleep End'].astype('datetime64[ms]')
+    raw_sleep.index = raw_sleep.index.date # change index from datetime to date
+    return raw_sleep
+
+

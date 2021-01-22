a9066b20aaaf41907439d8bd44f43952eba4959c
Joseph Pauly <49600051+jbpauly@users.noreply.github.com>
2021-01-22 09:42:12 -0600

    bug fix

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-from streamlit_pandas_profiling import st_profile_report
+# from streamlit_pandas_profiling import st_profile_report
 import utilities as util
 import plot
 import pandas as pd
